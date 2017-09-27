d695cb5336274d134972f6258cfc6ef5708cfbbf
Ken Kotch <hello@ken.uno>
2017-09-26 22:15:15 -0600

    get list works

--- routes/books.js
+++ routes/books.js
@@ -1,10 +1,35 @@
-'use strict';
+'use strict'
 
-const express = require('express');
+const express = require('express')
+const knex = require('../knex')
 
 // eslint-disable-next-line new-cap
-const router = express.Router();
+const router = express.Router()
 
-// YOUR CODE HERE
+// L
+router.get('/books', (req, res, next) => {
+  knex('books')
+    .select(
+      'id',
+      'title',
+      'author',
+      'genre',
+      'description',
+      'cover_url AS coverUrl',
+      'created_at AS createdAt',
+      'updated_at AS updatedAt'
+    )
+    .orderBy('title', 'ASC')
+    .then((items) => {
+      res.setHeader('Content-Type', 'application/json')
+      res.status(200)
+      res.send(JSON.stringify(items))
+    })
+    .catch((err) => next(err))
+})
 
-module.exports = router;
+
+
+
+
+module.exports = router
