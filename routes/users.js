dfc688f2861adc2d78fc7b1311907f7f7fa51a69
Ken Kotch <hello@ken.uno>
2017-09-27 23:34:53 -0600

    part 3: user reg complete

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,34 @@
 'use strict'
 
 const express = require('express')
-
+const knex = require('../knex')
+const bcrypt = require('bcrypt')
 const router = express.Router()
 
+router.post('/users', (req, res, next) => {
+  let thePassword = req.body.password
 
+  bcrypt.hash(thePassword, 12)
+  .then((result) => {
+  return knex('users')
+  .insert({
+    first_name: req.body.firstName,
+    last_name: req.body.lastName,
+    email: req.body.email,
+    hashed_password: result
+  }, '*')
+  .then((user) => {
+    const newObj = {
+      id: user[0].id,
+      firstName: user[0].first_name,
+      lastName: user[0].last_name,
+      email: user[0].email,
+      // hashedPassword: user[0].hashed_password
+    }
+    res.send(newObj)
+  })
+  .catch((err) => next(err))
+})
+})
 
 module.exports = router
