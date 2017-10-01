e63e24afb7c22ad4690ef42a3b42133256f2b121
Ken Kotch <hello@ken.uno>
2017-09-30 21:43:56 -0600

    /GET favorites with and without token pass tests

--- routes/favorites.js
+++ routes/favorites.js
@@ -1,15 +1,24 @@
 'use strict'
 
-const express = require('express')
-const knex = require('../knex')
 const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
 const boom = require('boom')
+const { camelizeKeys, decamelizeKeys } = require('humps')
+const jwt = require('jsonwebtoken')
+const knex = require('../knex')
+const express = require('express')
 const router = express.Router()
 const JWT_KEY = process.env.JWT_KEY
 
 // JWT authorize
-
+const authorize = (req, res, next) => {
+  jwt.verify(req.cookies.token, JWT_KEY, (err, payload) => {
+    if (err) {
+      return next(boom.create(401, 'Unauthorized'))
+    }
+    req.claim = payload
+    next()
+  })
+}
 
 // C
 
@@ -21,27 +30,14 @@ const JWT_KEY = process.env.JWT_KEY
 
 
 // L
-router.get('/favorites', (req, res, next) => {
-  jwt.verify(req.cookies.token, JWT_KEY, (err, payload) => {
-    if (err) {
-      next(boom.create(401, 'Unauthorized'))
-      return
-    } // else {
-    knex('favorites')
-      .select(
-        'id',
-        'book_id AS bookId',
-        'user_id AS userId',
-        'created_at AS createdAt',
-        'updated_at AS updatedAt'
-      )
-      // .orderBy('id', 'ASC')
-      .then((items) => {
-        // res.status(200)
-        res.json(items)
-      })
-    // }
-  })
+router.get('/favorites', authorize, (req, res, next) => {
+  knex('favorites')
+    .innerJoin('books', 'books.id', 'favorites.book_id')
+    .where('favorites.user_id', req.claim.userId)
+    .then((items) => {
+      const favs = camelizeKeys(items)
+      res.send(favs)
+    })
 })
 
 
